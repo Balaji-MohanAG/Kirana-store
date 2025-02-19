47ffe84fb62277efc5ada801a72a2c1bd182b08c
balajim <balaji@changejar.in>
2025-02-19 12:40:57 +0530

    partial refresh token

--- src/main/java/com/jarapplication/kiranastore/constants/SecurityConstants.java
+++ src/main/java/com/jarapplication/kiranastore/constants/SecurityConstants.java
@@ -2,4 +2,6 @@ package com.jarapplication.kiranastore.constants;
 
 public class SecurityConstants {
     public static final String SECRET_KEY = "yourSuperSecretKey_yourSuperSecretKey_yourSuperSecretKey_yourSuperSecretKey";
+    public static final String TOKEN_PREFIX = "Bearer ";
+        public static final String ACCESS_TOKEN_EXPIRATION_TIME = ;
 }
